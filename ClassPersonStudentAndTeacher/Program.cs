3b8474974e06e91ca1f0e3cf4b0a47fde83e8e82
Jian Xue <royjianxue@yahoo.com>
2022-05-12 16:35:03 -0400

    Add project files.

---
+++ ClassPersonStudentAndTeacher/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ClassPersonStudentAndTeacher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Greet("Hello!");
+
+            Student student = new Student();
+            student.Greet("Hello!");
+            student.SetAge(21);
+            student.ShowAge();
+
+            Professor professor = new Professor();
+            professor.Greet("Hello!");
+            professor.Explain();
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Person
+    {
+
+        public int Age { get; set; }
+
+        public void Greet(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void SetAge(int age)
+        {
+            Age = age;
+        }
+
+    }
+
+    public class Student : Person
+    {
+        public void study()
+        {
+            Console.WriteLine("I am studying.");
+        }
+
+        public void ShowAge()
+        {
+            Console.WriteLine($"My age is: {Age} years old");
+        }
+
+
+    }
+    public class Professor : Person
+    {
+        public void Explain()
+        {
+            Console.WriteLine("I am explaining.");
+        }
+    }
+
+
+
+}
